edf09dce7da229f76ef821be59ed73a3c8c03db0
asif <asifmai@post.bgu.ac.il>
2024-02-26 23:48:54 +0200

    finished mnist task, pixel by pixel works poorly

--- lstm_ae_mnist.py
+++ lstm_ae_mnist.py
@@ -68,17 +68,18 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using {device} device')
 
 parser = argparse.ArgumentParser(description='Train an autoencoder with a classifier on MNIST')
-parser.add_argument('--hidden_size', type=int, default=27, help='Size of the hidden layer')
-parser.add_argument('--num_layers', type=int, default=4, help='Number of layers in the LSTM')
-parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train the model')
-parser.add_argument('--optimizer', type=str, default='Adam', help='Optimizer to use')
-parser.add_argument('--learning_rate', type=float, default=0.01, help='Learning rate for the optimizer')
-parser.add_argument('--grad_clip', type=int, default=1, help='Gradient clipping value')
-parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
-parser.add_argument('--random', action='store_true' , help='Whether to use a random seed')
+parser.add_argument('-hs', '--hidden_size', type=int, default=27, help='Size of the hidden layer')
+parser.add_argument('-layers','--num_layers', type=int, default=4, help='Number of layers in the LSTM')
+parser.add_argument('-epo','--epochs', type=int, default=100, help='Number of epochs to train the model')
+parser.add_argument('-opt','--optimizer', type=str, default='Adam', help='Optimizer to use')
+parser.add_argument('-lr','--learning_rate', type=float, default=0.01, help='Learning rate for the optimizer')
+parser.add_argument('-gc','--grad_clip', type=int, default=1, help='Gradient clipping value')
+parser.add_argument('-bs','--batch_size', type=int, default=64, help='Batch size for training')
+parser.add_argument('-r','--random', action='store_true' , help='Whether to use a random seed')
+parser.add_argument('-pxl','--pixel_input', action='store_true', help='Whether to use pixel input or row input')
 args = parser.parse_args()
 
-input_size = output_size = 28
+input_size = output_size = 28 if not args.pixel_input else 1
 optimizer_dict = {'Adam': torch.optim.Adam, 'SGD': torch.optim.SGD, 'adagrad': torch.optim.Adagrad, 'adadelta': torch.optim.Adadelta}
 hidden_size = args.hidden_size
 num_layers =  args.num_layers
@@ -99,6 +100,11 @@ if not args.random:
 transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
                                 torchvision.transforms.Normalize((0.5,), (0.5,))])
 
+if args.pixel_input:
+    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                torchvision.transforms.Normalize((0.5,), (0.5,)),
+                                torchvision.transforms.Lambda(lambda x: torch.flatten(x))])
+
 
 #mnist_data is a tensor, it holds num_images X 2, where the first element is the image and the second is the label
 mnist_train_data = torchvision.datasets.MNIST('mnist_data', train=True, download=True, transform=transform)
@@ -116,9 +122,12 @@ test_labels = [label for image, label in mnist_test_data]
 #later turn this into an argparse apllication
 
 
-
-train_images = train_images.view(-1, 28, 28)
-test_images = test_images.view(-1, 28, 28)
+if not args.pixel_input:
+    train_images = train_images.view(-1, 28, 28)
+    test_images = test_images.view(-1, 28, 28)
+else:
+    train_images = train_images.view(-1, 784, 1)
+    test_images = test_images.view(-1, 784, 1)
 
 
 train_loader = DataLoader(train_images, batch_size=batch_size, shuffle=False)
@@ -156,11 +165,14 @@ for i in range(len(classifier_predictions)):
         correct += 1
 print(f'Accuracy: {correct/len(classifier_predictions)}')
 
+import random
 fig, axs = plt.subplots(2, 5)
-for i in range(10):
-    axs[i//5, i%5].imshow(predictions[i].detach().numpy())
-    axs[i//5, i%5].set_title(np.argmax(classifier_predictions[i].detach().numpy()))
-    axs[i//5, i%5].axis('off')
+sample_indices = [random.randint(0, len(predictions)) for i in range(10)]
+for j,i in enumerate(sample_indices):
+    axs[j//5, j%5].imshow(predictions[i].view(28,28).detach().numpy())
+    axs[j//5, j%5].set_title(np.argmax(classifier_predictions[i].detach().numpy()))
+    axs[j//5, j%5].axis('off')
+plt.savefig('reconstructed_images.png')
 plt.show()
 
 
